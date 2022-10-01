02a115aefd396c024bd3cf3fa539d894836643fc
ilyatyulenev <seal-99@mail.ru>
2022-10-01 20:54:16 +0300

    Added StartGameView.vue component

---
+++ src/components/StartGameView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="$style.gameView">
+    <slot name="inputs" :class="$style.inputs"/>
+
+    <StartButton/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import StartButton from '@/components/StartButton.vue';
+
+@Options({
+  components: {
+    StartButton,
+  },
+})
+export default class StartGameView extends Vue {
+}
+</script>
+
+<style module>
+  .gameView {
+    width: 500px;
+    text-align: center;
+  }
+
+  .inputs {
+    margin-bottom: 40px;
+  }
+</style>
